593f6d67366e520793e6b67deac229e27f006aff
Joohhnnyyy <johnsonansh32@gmail.com>
2026-01-23 19:31:56 +0530

    making of react folder

---
+++ part46/Project1/src/App.jsx
@@ -0,0 +1,19 @@
+// const app = () => <div>
+//   <h1>Hello, World!</h1>
+//   <h2>testing script</h2>
+//   </div>;
+//   export default app;
+  
+
+
+
+const  App = () => {
+  return (
+    <div>
+     <h1>Hello World</h1>
+     <h2>testing script</h2>
+    </div>
+  )
+}
+
+export default App
