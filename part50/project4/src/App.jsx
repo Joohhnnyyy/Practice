3437d899c3bea0645bdc33f5fc6a2f177dfa656a
Joohhnnyyy <johnsonansh32@gmail.com>
2026-01-26 17:51:52 +0530

    message

---
+++ part50/project4/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../components/Card.jsx'
+import Hero from '../components/Hero.jsx'
+
+const App = () => {
+
+
+
+  return (
+    <div >
+      <Hero />
+      </div>
+  )
+}
+
+export default App
