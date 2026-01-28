470458e9cadf5447651160535a646149c4876e74
Joohhnnyyy <johnsonansh32@gmail.com>
2026-01-29 01:58:52 +0530

    getting hands on useState

--- part50/project4/src/App.jsx
+++ part50/project4/src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Card from '../components/Card.jsx'
-import Hero from '../components/Hero.jsx'
 
 const App = () => {
-
-
-
   return (
-    <div >
-      <Hero />
-      </div>
+    <div>App</div>
   )
 }
 
